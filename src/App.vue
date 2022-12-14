b29e26cc89b0262715deeeb76e759f78a791ce62
abmysong <abmysong@gmail.com>
2022-12-14 19:32:33 +0900

    [#1] 마크업

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <!-- HEADER -->
+  <header>
+    <div class="inner">
+      <h1><a href="/">프레임마스터</a></h1>
+      <ul>
+        <li><a href="javascript:void(0)">안경</a></li>
+        <li><a href="javascript:void(0)">선글라스</a></li>
+        <li><a href="javascript:void(0)">악세사리</a></li>
+        <li><a href="./serviceCenter.html">고객센터</a></li>
+      </ul>
+      <div>
+        <a href="./login.html">로그인</a>
+      </div>
+    </div>
+  </header>
+  <!-- SECTION -->
+  <section>
+    <div class="inner">
+      이미지 준비중입니다.
+    </div>
+  </section>
+  <!-- FOOTER -->
+  <Footer>
+    <div class="footer-background">
+      <div class="inner">
+        <div class="footer-left">
+          푸터 왼쪽
+        </div>
+        <div class="footer-right">
+          푸터 오른쪽
+        </div>
+      </div>
+    </div>
+  </Footer>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import store from './store'
+import '@/assets/scss/index.css'
+import '@/assets/scss/login.css'
+import '@/assets/scss/serviceCenter.css'
+import '@/assets/scss/serviceCenterWrite.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+.use(store)
+// .use(router)
+.mount('#app')
